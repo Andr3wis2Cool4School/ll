0c98b4982a66935a406d513bf3df02dfd8069c15
andrew <20448589@life.hkbu.edu.hk>
2021-01-17 09:13:45 +0800

    15

--- 15.py
+++ 15.py
@@ -1,5 +1,28 @@
 def threeSum(nums):
-    if len(nums) < 3:
+    n = len(nums)
+    res = []
+    if nums is None and n < 3:
         return []
+    nums.sort()
+    for i in range(n):
+        if nums[i] > 0:
+            return res
+        if i > 0 and nums[i] == nums[i-1]:
+            continue
+        L = i + 1
+        R = n - 1
+        while L < R:
+            if nums[i] + nums[L] + nums[R] == 0:
+                res.append([nums[i], nums[L], nums[R]])
+                while L < R and nums[L] == nums[L+1]:
+                    L += 1
+                while L < R and nums[R] == nums[R-1]:
+                    R -= 1
+                L += 1
+                R -= 1
+            elif nums[i] + nums[L] + nums[R] > 0:
+                R -= 1
+            else:
+                L += 1
 
-
+    return res
